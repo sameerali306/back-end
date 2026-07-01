15052ae4769d1e11c2ab37c21363da7cfdc0de29
Sameer Aly <sameeraly240@gmail.com>
2026-07-01 18:39:11 +0500

    router

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,40 +1,43 @@
 import express from 'express'
 
-const app=express();
-app.get('/',(req,res)=>{
+const app = express();
+
+app.get('/', (req, res) => {
     res.send('server is ready..');
+});
 
-})
 
-app.get('/api/jokes',(req,res)=>{
-    const jokes=[
+app.get('/api/jokes', (req, res) => {
+
+    const jokes = [
         {
-            id:1,
-            title:"the first joke",
-            description:"hahaha"
+            id: 1,
+            title: "the first joke",
+            description: "hahaha"
         },
         {
-            id:2,
-            title:"the second joke",
-            description:"hello"
+            id: 2,
+            title: "the second joke",
+            description: "hello"
         },
         {
-            id:3,
-            title:"the third joke",
-            description:"hi"
+            id: 3,
+            title: "the third joke",
+            description: "hi"
         },
         {
-            id:4,
-            title:"the fourth joke",
-            description:"lalalala"
+            id: 4,
+            title: "the fourth joke",
+            description: "lalalala"
         }
-    ]
-    res.send(jokes)
-})
+    ];
 
-const port=process.env.PORT ||5000;
+    res.send(jokes);
+});
 
-app.listen(port,()=>{
+
+const port = 6151;
+
+app.listen(port, () => {
     console.log(`server run at http://localhost:${port}`);
-    
-})
+});
